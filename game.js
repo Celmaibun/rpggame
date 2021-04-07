89a08aad42713f0bbb636c068b6291435b42e1da
Karol Skalik <48031760+Celmaibun@users.noreply.github.com>
2021-04-07 22:14:13 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -43,35 +43,35 @@ const textNodes = [
         text: 'Dobra, zacznijmy od początku, bo nie wiem czy znasz się na rzeczy. Kojarzysz krypto i wiesz o co biega z inwestycjami?',
         options: [
             {
-                text: 'noo..mniej więcej rozumiem',
+                text: 'Noo..mniej więcej rozumiem',
                 nextText: 2
             },
             {
-                text: 'wiadomo, jestem mistrzem inwestycji',
+                text: 'Wiadomo, jestem mistrzem inwestycji',
                 nextText: 3
             },
             {
-                text: 'ee..no tak średnio, powiesz coś więcej?',
+                text: 'Eee..no tak średnio, powiesz coś więcej?',
                 nextText: 4
             }
         ]
     },
     {
         id: 2,
-        text: 'w dużym skrócie, bierzesz hajs, obczajasz krypto, sprawdzasz jak wygląda sytuacja z daną firmą co wprowadziła dane krypto i inwestujesz jak jest na dipie i hold, bo jak wypłacisz to rynek się jebie i przez ciebie umrą ludzie.',
+        text: 'W dużym skrócie, bierzesz hajs, obczajasz krypto, sprawdzasz jak wygląda sytuacja z daną firmą co wprowadziła dane krypto i inwestujesz jak jest na dipie i hold, bo jak wypłacisz to rynek się jebie i przez ciebie umrą ludzie.',
         options: [
             {
-                text: 'okej?',
+                text: 'Okej?',
                 nextText: 5
             }
         ]
     },
     {
         id: 3,
-        text: 'dobra nie pierdol i tak pewnie więcej przejebałeś niż zarobiłeś',
+        text: 'Dobra nie pierdol i tak pewnie więcej przejebałeś niż zarobiłeś',
         options: [
             {
-                text: 'okej?',
+                text: 'Chuja wiesz zjebie, ale dobra',
                 nextText: 5
             }
         ]
@@ -81,7 +81,7 @@ const textNodes = [
         text: 'Dobra mniejsza, wszystko wyjdzie w praktyce',
         options: [
             {
-                text: 'okej?',
+                text: 'Okej?',
                 nextText: 5
             }
         ]
@@ -127,12 +127,12 @@ const textNodes = [
     },
     {
         id: 7,
-        text: '-Dobra, to przelejj 500zł i lecimy dalej\r' +
-        '- em...Dobra już, to kiedy mogę liczyć na zysk?\r' +
-        '- noo w dobrym tempie za 3-4 dni będziesz mieć efekt\r'+
-        '-Szybciej niż się spodziewałem\r'+
-        '-Mówiłem ci, giełda to dynamiczna kobieta, nie lubi stać w miejscu, ale nie raz cię jeszcze wyrucha\r'+
-        '-eee..ok?\r\r\r'+
+        text: '- Dobra, to przelejj 500zł i lecimy dalej\r\r' +
+        '- em...Dobra już, to kiedy mogę liczyć na zysk?\r\r' +
+        '- noo w dobrym tempie za 3-4 dni będziesz mieć efekt\r\r'+
+        '- Szybciej niż się spodziewałem\r\r'+
+        '- Mówiłem ci, giełda to dynamiczna kobieta, nie lubi stać w miejscu, ale nie raz cię jeszcze wyrucha\r\r'+
+        '- eee..ok?\r\r\r'+
         '- Dobra to teraz idę się zająć gotowaniem, wiesz.. muriatic acid i takie tam, znasz się na chemii?',
         options: [
             {
@@ -159,9 +159,9 @@ const textNodes = [
     ////////////////////////////////////////////////////////////////////
     {
         id: 9,
-        text: '-W sumie brzmi sensownie, kuźwa jak syczy, powinno być tak?\r'+
-        '-Mówię przecież, że musi dojść do reakcji\r'+
-        '-Dobra kurwa, pierwszy raz to gotuję...o jest git, dobra dzięki, bo się bałem, że spierdolę. Dobra to widzimy się jutro, pamiętaj być punktualnie, bo szef wpadnie to musi być wszystko ideolo.\r'
+        text: '- W sumie brzmi sensownie, kuźwa jak syczy, powinno być tak?\r\r'+
+        '- Mówię przecież, że musi dojść do reakcji\r\r\r'+
+        '- Dobra kurwa, pierwszy raz to gotuję...o jest git, dobra dzięki, bo się bałem, że spierdolę. Dobra to widzimy się jutro, pamiętaj być punktualnie, bo szef wpadnie to musi być wszystko ideolo.\r'
         ,
         options: [
             {
@@ -174,19 +174,66 @@ const textNodes = [
             },
         ]
     },
+    {
+        id: 11,
+        text: 'Mija tydzień. Zjawiasz się w pracy punktualnie. Wchodzisz do środka i pukasz do drzwi. Otwiera ci wielki, łysy chłop, który ma więcej żył na łapie niż twoja stara fałd na dupie. Robisz kilka kroków i twoim oczom ukazuje się szef, Również dobrze zbudowany mężczyzna, włosy spięte w kok, biały garnitur i wielki glock na biurku. Przed nim siedzi Wiktor - twój kumpel, z którym ostatnio rozmawiałeś. Gestem ręki szef zaprasza cię do siebie, więc zajmujesz wskazane ci miejsce.',
+        options: [
+            {
+                text: 'To po co tu jesteśmy?',
+                nextText: 12,
+            },
+            {
+                text: 'Rozumiem, że spotkaliśmy się w interesach, a nie dla pogawędek?',
+                nextText: 13,
+            },
+        ]
+    },
+    {
+        id: 12,
+        text: '- Jak pewnie pamiętasz ostatnio z Wiktorem przygotowywaliście dla mnie towar i wyszedł on..zadziwiająco dobry, więc jestem skłonny kontynuować z tobą współpracę w tej kwestii.\r\r'+
+        '- To znaczy?\r\r'+
+        '- To znaczy, że możesz będziesz gotować z Wiktorem, czyli ty robisz swoje, a my w zamian płacimy ci oraz masz te same przywileje co Wiktor.\r\r'+
+        '- A jeśli odmówię?\r\r'+
+        '- Chyba się nie zrozumieliśmy, już zaczęliśmy współpracę, od ciebie zależy, czy ją kontynuujemy, czy ją zakończymy. Podpowiem tylko, że zakończenie tej współpracy skończyłoby się nie najlepiej dla ciebie.\r\r'
+        ,
+        options: [
+            {
+                text: 'Rozumiem...',
+                nextText: 14,
+            },
+        ]
+    },
+    {
+        id: 13,
+        text: '- Jak pewnie pamiętasz ostatnio z Wiktorem przygotowywaliście dla mnie towar i wyszedł on..zadziwiająco dobry, więc jestem skłonny kontynuować z tobą współpracę w tej kwestii.\r\r'+
+        '- To znaczy?\r\r'+
+        '- To znaczy, że możesz będziesz gotować z Wiktorem, czyli ty robisz swoje, a my w zamian płacimy ci oraz masz te same przywileje co Wiktor, ale wiadomo, odejście z firmy nie wchodzi w grę.\r\r',
+        
+        options: [
+            {
+                text: 'Rozumiem',
+                nextText: 14,
+            },
+        ]
+    },
+    {
+        id: 14,
+        text: 'W świetle takich warunków nie pozostało ci nic innego jak przystać na takie warunki, kontynuowałeś współpracę z szefem Wiktora, a raczej z waszym szefem i wiodło ci się coraz lepiej. Co do krypto to straciłeś niemal całość, bo przez następne tygodnie miałeś tylko czerwoną świecę, aż w końcu usunąłeś apkę',
+    },
 
-    
+
+/////////////////////////////////////////////////////////////////////////////////////////
     {
         id: 10,
-        text: '-Hmm...No skoro tak mówisz...Ty, a powinno to się tak kopcić?\r'+
-        '-No..zachodzi reakcja katalizacji i teraz się łączą obie substancje\r'+
-        '-Kurwa jak jebie, ja pierdole cała partia w pizdu, zobacz jak to dymi skurwysyństwo!\r'+
-        '-Nie o to ci chodziło?\r'+
-        '-kurwa ja tu mete gotuję, a dym czarny jak twoja stara gdy wraca z piwnicy niosąc węgiel\r'+
-        '-Chciałeś pomocy to ci powiedziałem co i jak\r'+
-        '- I żałuję, bo cała partia w pizdu, dobra zejdź mi z oczu, muszę wykombinować jak to teraz naprawić, bo mnie zajebią chłopaki…\r'+
-        '-To kiedy się widzimy?\r'+
-        '-Za tydzień jak zawsze, bądź punktualnie, albo nie przychodź wcale, bo pewnie nie dożyję następnego tygodnia dzięki tobie..\r'
+        text: '- Hmm...No skoro tak mówisz...Ty, a powinno to się tak kopcić?\r\r'+
+        '- No..zachodzi reakcja katalizacji i teraz się łączą obie substancje\r\r'+
+        '- Kurwa jak jebie, ja pierdole cała partia w pizdu, zobacz jak to dymi skurwysyństwo!\r\r'+
+        '- Nie o to ci chodziło?\r\r'+
+        '- Kurwa ja tu mete gotuję, a dym czarny jak twoja stara gdy wraca z piwnicy niosąc węgiel\r\r'+
+        '- Chciałeś pomocy to ci powiedziałem co i jak\r\r'+
+        '- I żałuję, bo cała partia w pizdu, dobra zejdź mi z oczu, muszę wykombinować jak to teraz naprawić, bo mnie zajebią chłopaki…\r\r'+
+        '- To kiedy się widzimy?\r\r'+
+        '- Za tydzień jak zawsze, bądź punktualnie, albo nie przychodź wcale, bo pewnie nie dożyję następnego tygodnia dzięki tobie..\r\r'
         ,
         options: [
             {
@@ -194,6 +241,73 @@ const textNodes = [
                 nextText: 100,
             },
         ]
-    }
+    },
+
+    {
+        id: 100,
+        text: '- Ehh..no nic, to poszukam tego przepisu, mam nadzieję, że się wyrobię w godzinę z tym wszystkim\r\r'+
+        '- Dobra to ja się będę zbierał\r\r'+
+        '- Jasne, widzimy się za tydzień, tylko pamiętaj, żeby być punktualnie, bo szef cie zajebie\r\r'
+        ,
+        options: [
+            {
+                text: 'Rozumiem, do następnego.',
+                nextText: 101,
+            },
+            {
+                text: 'Dobra spierdalaj',
+                nextText: 101,
+            },
+        ]
+    },
+    {
+        id: 101,
+        text: 'Mija tydzień. Zjawiasz się w pracy punktualnie. Niestety nie widzisz, żeby twój kumpel też się zjawił. Wchodzisz do środka i pukasz do drzwi. Otwiera ci wielki, łysy chłop, który ma więcej żył na łapie niż twoja stara fałd na dupie. Robisz kilka kroków i twoim oczom ukazuje się szef. Również dobrze zbudowany mężczyzna, włosy spięte w kok, biały garnitur i wielki glock na biurku.  Gestem ręki zaprasza cię do siebie, więc zajmujesz wskazane ci miejsce.'
+        ,
+        options: [
+            {
+                text: 'Gdzie jest Wiktor? Ten koleś z którym pracowałem?',
+                nextText: 102,
+            },
+            {
+                text: 'Co jest powodem naszego spotkania?',
+                nextText: 103,
+            },
+        ]
+    },
+    {
+        id: 102,
+        text: '- Wiktor nie wywiązał się ze swojej części umowy, więc my też tego nie zrobiliśmy.\r\r'+
+        '- A dokładniej?\r\r'+
+        '- Miał nam dostarczyć 300g metamfetaminy tydzień temu, a w zamian oferowaliśmy mu ochronę. Nie dostarczył towaru, nie było ochrony, nie było ochrony..cóż..dzisiejszy świat bywa ciężki.\r\r'
+        
+        ,
+        options: [
+            {
+                text: '...',
+                nextText: 105,
+            },
+        ]
+    },
+    {
+        id: 103,
+        text: '- Jak się domyślasz, nie jesteśmy fundacją charytatywną tylko poważną organizacją i jeśli coś wyjąć to najpierw musisz coś włożyć. Współpracujesz z nami już trochę czasu i miałeś kilka możliwości się wykazać\r\r'+
+        '- Przecież nic od was nie brałem, ani nie korzystałem z waszych usług\r\r'+
+        '- Wręcz przeciwnie, po pierwsze Wiktor z nami współpracował i dowiedzieliśmy się o tym co jest powodem jego niepowodzenia związanego z produkcją i wiemy też, że maczałeś w tym palce.\r\r'+
+        '- Jak ja nic nie zrobiłem, on miał gotować i pytał mnie o rady, przecież to na jego głowie było zajęcie się towarem, nie mojej\r\r'+
+        '- Z tego co nam mówił odegrałeś znaczącą rolę w przygotowaniu towaru, więc też za to odpowiadasz, on już swoją cenę zapłacił jak się domyślasz po jego...nieobecności, czas na ciebie, masz dwie opcje. Albo zapłacisz za towar 700zł, tu i teraz, albo spotkasz się z Wiktorem w pobliskim jeziorze, a tego byśmy raczej nie chcieli.\r\r'
+        
+        ,
+        options: [
+            {
+                text: '...',
+                nextText: 105,
+            },
+        ]
+    },
+    {
+        id: 105,
+        text: '. Przypomina ci się, że Wiktor uczył cię inwestowania, więc wyciągasz telefon i sprawdzasz ile zarobiłeś. Patrzysz na saldo w portfelu…”-50% w ciągu ostatniego tygodnia”..masz na koncie ledwo 250zł i kwota ciągle spada. Krótko mówiąc jesteś w czarnej dupie i właśnie pod kamienicę podjechał samochód, który za chwilę zabierze twoje zwłoki na małą wycieczkę'
+    },
 ]
 startGame();
